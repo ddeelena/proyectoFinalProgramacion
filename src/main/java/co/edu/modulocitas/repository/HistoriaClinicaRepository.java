e6230de07296d56223ae389cad82b628c0a8ccb5
nicolasmdssal <nmorales_6@cue.edu.co>
2025-05-22 15:49:03 -0500

    Avances creacion de citas

---
+++ src/main/java/co/edu/modulocitas/repository/HistoriaClinicaRepository.java
@@ -0,0 +1,7 @@
+package co.edu.modulocitas.repository;
+
+import co.edu.modulocitas.model.HistoriaClinica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HistoriaClinicaRepository extends JpaRepository<HistoriaClinica, Integer> {
+}
